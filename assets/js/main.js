f7417b28aef2e4b270ecb42ee0e9512eb72eb25f
manjurulalammahi <manjurulalammahi@gmail.com>
2025-04-27 18:13:05 +0600

    first

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+$(function () {
+    'use strict';
+    
+    // Nav Fix
+    // var navoffset = $('#main_nav').offset().top;
+
+    // $(window).on('scroll', function () {
+    //     var nav = $(this).scrollTop();
+    //     if (nav > navoffset) {
+    //         $('#main_nav').addClass('fixed_nav');
+    //     } else {
+    //         $('#main_nav').removeClass('fixed_nav');
+    //     }
+    // });
+
+});
